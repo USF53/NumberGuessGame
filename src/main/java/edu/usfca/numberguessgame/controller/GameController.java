1eceb4d6ad13613ad605616b25d062f0476cde92
crsmith20 <31359448+crsmith20@users.noreply.github.com>
2021-10-18 22:26:11 -0700

    changed for view example

--- src/main/java/edu/usfca/numberguessgame/controller/GameController.java
+++ src/main/java/edu/usfca/numberguessgame/controller/GameController.java
@@ -2,25 +2,41 @@ package edu.usfca.numberguessgame.controller;
 
 import edu.usfca.numberguessgame.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+//@RestController
+@Controller
 public class GameController {
 
     @Autowired
     GameService gameService;
+    
+    // Main View
+    @GetMapping(value = { "", "/" })
+    public String main(Model model) {
+    	return "main";
+    }
 
-    @RequestMapping(value = "/setBound", method = RequestMethod.GET)
+    // Should return the "guess" view
+    @RequestMapping(value = "/setBound", method = RequestMethod.POST)
     public String setBound(@RequestParam String lowerBound, @RequestParam String upperBound) {
+    	
         return gameService.handleSetBound(lowerBound, upperBound);
     }
 
-    @RequestMapping(value = "/guess", method = RequestMethod.GET)
-    public String guess(@RequestParam String number) {
+    // If the answer is correct then return "congrats" view otherwise return "guess" view again
+    @ResponseBody
+    @RequestMapping(value = "/guess", method = RequestMethod.POST)
+    public String guess(Model model, @RequestBody String number) {
         return gameService.handleGuess(number);
     }
 }
