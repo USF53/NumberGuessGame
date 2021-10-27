d028e77f63f7166251a79f18ab3d83c73bd5dcb8
xsun418 <xsun37@usfca.edu>
2021-10-27 13:11:36 -0700

    delete jasypt

--- src/main/java/edu/usfca/numberguessgame/NumberGuessGameApplication.java
+++ src/main/java/edu/usfca/numberguessgame/NumberGuessGameApplication.java
@@ -1,6 +1,5 @@
 package edu.usfca.numberguessgame;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
