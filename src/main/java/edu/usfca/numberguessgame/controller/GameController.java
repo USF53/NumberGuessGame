92b7dad0b1dfa31427ad21f5960635bc82245f04
xsun418 <xsun37@usfca.edu>
2021-11-15 01:52:23 -0800

    discord support

--- src/main/java/edu/usfca/numberguessgame/controller/GameController.java
+++ src/main/java/edu/usfca/numberguessgame/controller/GameController.java
@@ -1,6 +1,7 @@
 package edu.usfca.numberguessgame.controller;
 
 import edu.usfca.numberguessgame.model.GuessRequest;
+import edu.usfca.numberguessgame.model.Session;
 import edu.usfca.numberguessgame.model.SetBoundRequest;
 import edu.usfca.numberguessgame.service.GameService;
 
@@ -9,13 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GameController {
 
     @Autowired
-    GameService gameService;
+    private GameService gameService;
 
     // Main View
     @GetMapping(value = {"", "/"})
@@ -24,13 +24,33 @@ public class GameController {
     }
 
     @PostMapping (value = "/setBound")
-    public String setBound(SetBoundRequest request,Model model) {
-        return gameService.handleSetBound(request.getLowerBound(), request.getUpperBound(),model);
+    public String setBound(SetBoundRequest request, Model model) {
+        String page;
+        try {
+            Session session = gameService.handleSetBound(request.getLowerBound(), request.getUpperBound());
+            model.addAttribute("bound", "Your Input Is Valid. Please Try To Guess It!");
+            model.addAttribute("currId", session.getId());
+            page = "guess";
+        } catch (Exception e) {
+            // log exception for visibility
+            model.addAttribute("bound", e.getMessage());
+            page = "main";
+        }
+        return page;
     }
 
     @PostMapping("/guess")
-    public String guess(GuessRequest request,Model model) {
-        return gameService.handleGuess(request,model);
+    public String guess(GuessRequest request, Model model) {
+        String page;
+        try {
+            model.addAttribute("currId", request.getCurrId());
+            gameService.handleGuess(request.getCurrId(), request.getNumber());
+            page = "congrats";
+        } catch (Exception e) {
+            model.addAttribute("guess", e.getMessage());
+            page = "guess";
+        }
+        return page;
     }
 }
 
